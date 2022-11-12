ce888c47dc1bbbb145746f2ad47db47416acf97d
Cyber_Infernox <sayonkardpsm12a@gmail.com>
2022-11-12 10:54:16 +0530

    #4 - Dynamic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  // const person = { name: "Yoshi", age: 30 };
+  const go = "http://www.google.com";
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -20,7 +25,15 @@ function App() {
       </header> */}
 
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"Hello Ninja's"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={go}>Google Site</a>
       </div>
     </div>
   );
