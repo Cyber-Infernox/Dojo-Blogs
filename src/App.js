40a972ad19046db68a9c438f22a4544af09c6f5e
Cyber_Infernox <sayonkar@gmail.com>
2022-11-27 17:13:35 +0530

    React Router
    Home route

--- src/App.js
+++ src/App.js
@@ -2,33 +2,22 @@
 // import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // A small change
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
